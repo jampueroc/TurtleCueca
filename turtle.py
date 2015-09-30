5e15402cd0f4b71f9e1561192ddba5d724c8c493
jampueroc <jorge.ampuero.1a@gmail.com>
2015-09-30 14:12:49 -0400

    Hace la intro de la cueca!

--- turtle.py
+++ turtle.py
@@ -8,51 +8,50 @@ from sensor_msgs.msg import JointState
 posI=posD=0
 
 def dance():
-    pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=10)
-    sub = rospy.Subscriber('joint_states', JointState)
-    rospy.init_node('dance', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    cuecaIntro(pub)
-    while not rospy.is_shutdown():
-	    rate.sleep()
+	pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=10)
+	sub = rospy.Subscriber('joint_states', JointState,callback)
+	rospy.init_node('dance', anonymous=True)
+	rate = rospy.Rate(10) # 10hz
+	cuecaIntro(pub)
+	while not rospy.is_shutdown():
+		rate.sleep()
 
 def cuecaIntro(pub):
-	caminar(pub)
-	darVuelta(pub)
-	#caminar(pub)
-	##darVuelta(pub)
-	#caminar(pub)
+	for i in range (4):
+		caminar(pub)
+		darVuelta(pub)
 
 def caminar(pub):
 	twist = Twist()
-	state= JointState()
-        print state.position
-        posI=state.position[0]
-        posD=state.position[1]
+	print state.position
+	posI=state.position[0]
+	posD=state.position[1]
 	#for i in range (100):
 	while abs(state.position[0]-posI)<28 and abs(state.position[1]-posD)<28:
 		twist.linear.x=0.2
 		pub.publish(twist)
 		rospy.sleep(0.05)
-	twist.linear.x=0
-	pub.publish(twist)
+		twist.linear.x=0
+		pub.publish(twist)
 
 def darVuelta(pub):
 	twist=Twist()
-	state= JointState()
-        posI=state.position[0]
-        posD=state.position[1]
-	while abs(state.position[0]-posI)<13 and abs(state.position[1]-posD)<12:
+	posI=state.position[0]
+	posD=state.position[1]
+	while abs(state.position[0]-posI)<10.5 and abs(state.position[1]-posD)<10.5:
 		twist.angular.z=0.32
 		pub.publish(twist)
 		rospy.sleep(0.1)
-	twist.angular.z=0
-	pub.publish(twist)	
-	
+		twist.angular.z=0
+		pub.publish(twist)	
+
+def callback(data):
+	global state
+	state=data
 
 if __name__ == '__main__':
-    try:
-        dance()
-    except rospy.ROSInterruptException:
-        pass
+	try:
+		dance()
+	except rospy.ROSInterruptException:
+		pass
 
